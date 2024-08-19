589f8d646468d2aa1370b471c1f173a091e0c23b
lifeadventurer <life0adventurer@gmail.com>
2024-08-19 23:50:35 +0800

    Refactor: Format repository details in marrkdown table view

--- main.py
+++ main.py
@@ -25,11 +25,21 @@ def get_remote_url(repository_path: str) -> str | None:
 
 def main():
     start_dir = os.getcwd()
+    repository_table_rows = [
+        "| Repository Name | Remote URL | Repository Path |",
+        "| ---- | ---- | ---- |",
+    ]
     for repository_path in find_git_repositories(start_dir):
         remote_url = get_remote_url(repository_path)
         if remote_url:
             repository_name = os.path.basename(repository_path)
-            print(f"| {repository_name} | {remote_url} | {repository_path} |")
+            repository_table_rows.append(
+                f"| {repository_name} | {remote_url} | {repository_path} |"
+            )
+
+    repository_table = "\n".join(repository_table_rows)
+
+    print(repository_table)
 
 
 if __name__ == "__main__":
